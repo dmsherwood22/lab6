21377436c1091da7bb059972c387a0bbd7b8ad7a
dmsherwood22 <dsherwoo@ucsd.edu>
2016-02-11 21:50:30 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,13 +20,25 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, addProject);
+
+}
+
+function addProject(result) {
+	console.log(result);
+
+	var projectHTML = '<a href = "#" class = "thumbnail">' + 
+	'<img src ="' + result['image'] + '"class = "detailsImage">' + '<p>' + result['title'] + '</p>' + 
+	'<p><small>' + result['date'] + '</small></p>' + '<p>' + result['summary'] + '</p></a>';
+	console.log(projectHTML);
+
+	$("#project" + result['id']).html(projectHTML);
+
 }
 
 /*
@@ -35,4 +47,5 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", function);
 }
